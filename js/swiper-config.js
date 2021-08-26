6bfd6cee52e292d5ff1935c7f4f75f0b287cc2af
Oleg <oleg_g87@outlook.com>
2021-08-26 14:10:08 +0300

    changed implementation of margins

--- js/swiper-config.js
+++ js/swiper-config.js
@@ -2,15 +2,14 @@ import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
 
 const swiper = new Swiper('.swiper-container', {
     direction: 'horizontal',
+
     loop: true,
     
     pagination: {
         el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
     },
-
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
 
     autoplay: {
         delay: 3000,
