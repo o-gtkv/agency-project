5aafe6219e7cc20094cb0ebd5bfc60ce459b78d4
Oleg <oleg_g87@outlook.com>
2021-09-13 16:44:23 +0300

    added overlay for the search form

--- src/js/bundle/main.bundle.js
+++ src/js/bundle/main.bundle.js
@@ -1,20 +1,25 @@
 class SearchForm {
     constructor() {
-        this.form = document.querySelector('.form-search');
+        this.form = document.querySelector('#form-search');
         this.textField = document.querySelector('.form-search__text-field');
         this.closeButton = document.querySelector('.form-search__close-button');    
-        this.closeButton.addEventListener('click', (e) => {
-            e.preventDefault();
-            this.hide();
+
+        this.form.addEventListener('click', (e) => {                              
+            const classes = ['form-search__close-button', 'fa-close', 'overlay'];
+            if (classes.some((value) => e.target.classList.contains(value))) {
+                this.hide();
+                e.preventDefault();
+            }
         });    
     }
 
     show = () => {
-        this.form.classList.add('form-search--visible');        
+        this.form.classList.add('display-block');        
     }
 
     hide = () => {
-        this.form.classList.remove('form-search--visible');        
+        this.form.classList.remove('display-block');
+        this.textField.value = '';
     }
 
     focus = () => {
